02a7cceed3580241b92c45bdb46d757029754573
Aniket Yadav <aniketyadav690@gmail.com>
2023-12-05 21:10:21 +0530

    api calling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import logo from "./logo.svg";
+import { useEffect, useContext } from "react";
 import { Chart, ChartSvg } from "./assests";
 import { Header, Header2, Search, Statbox } from "./components";
 import "./App.css";
+import { DataContext } from "./components/DataContext";
+
 
 function App() {
+  const { getBlockInformation, blocksData, transactions } = useContext(DataContext);
+
+  useEffect(() => {
+    getBlockInformation();
+    console.log("blocksData:", blocksData);
+    console.log("transactions:", transactions);
+}, []);
+
   return (
     <>
       <Header />
---
+++ src/components/DataContext.js
@@ -0,0 +1,54 @@
+import React from "react";
+import { useState } from "react";
+
+import alchemy from "../api/alchemy";
+
+const DataContext = React.createContext();
+
+function DataContextProvider({ children }) {
+  const [blocksData, setBlocksData] = useState([]);
+  const [transactions, setTransactions] = useState([]);
+
+  async function getBlockInformation() {
+    const blockNumber = await alchemy.core.getBlockNumber();
+
+    for (let i = 0; i < 20; i++) {
+      const info = await alchemy.core.getBlockWithTransactions(blockNumber - i);
+
+      const blockObject = {
+        number: info.number,
+        timestamp: info.timestamp,
+        miner: info.miner,
+        gasUsed: info.gasUsed.toString(),
+        gasLimit: info.gasLimit.toString(),
+        baseFeeInGwei: info.baseFeePerGas.toString() / 1000000000,
+        extraData: info.extraData,
+        hash: info.hash,
+        parentHash: info.parentHash,
+        nonce: info.nonce,
+        difficulty: info.difficulty,
+        transactions: info.transactions,
+      };
+
+      setBlocksData((prevData) => {
+        return [...prevData, blockObject];
+      });
+
+      if (i == 0) {
+        setTransactions((prevData) => {
+          return [...prevData, ...blockObject.transactions];
+        });
+      }
+    }
+  }
+
+  return (
+    <DataContext.Provider
+      value={{ getBlockInformation, blocksData, transactions }}
+    >
+      {children}
+    </DataContext.Provider>
+  );
+}
+
+export { DataContextProvider, DataContext };
