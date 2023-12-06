bcb99e85a74260d40527198ee4ce260a4a57f5a5
Aniket Yadav <aniketyadav690@gmail.com>
2023-12-06 20:00:55 +0530

    Pagination added (Blocks and Transactions)

--- src/components/Blocks.jsx
+++ src/components/Blocks.jsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext, useState } from "react";
 import moment from "moment";
 import { DataContext } from "../context/DataContext";
 import { Link } from "react-router-dom";
@@ -7,11 +7,18 @@ function Blocks() {
   const { blocksData } = useContext(DataContext);
   const [formattedBlocks, setFormattedBlocks] = useState([]);
 
-  const blocks = blocksData.map((blockData, index) => {
-    // const timestamp = moment(blockData.timestamp).valueOf();
-    // const age = moment(timestamp).fromNow();
-    const timestamp = moment(blockData.timestamp * 1000); // Convert to milliseconds
-    const formattedTimestamp = timestamp.format('LLL'); // Format timestamp in "LLL" format
+  const itemsPerPage = 10;
+  const totalPages = Math.ceil(blocksData.length / itemsPerPage);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+
+  const paginatedBlocks = blocksData.slice(startIndex, endIndex);
+
+  const blocks = paginatedBlocks.map((blockData, index) => {
+    const timestamp = moment(blockData.timestamp * 1000);
+    const formattedTimestamp = timestamp.format("LLL");
     const age = timestamp.fromNow();
     return (
       <div key={index} className="flex py-4">
@@ -46,12 +53,51 @@ function Blocks() {
         <p className="w-2/12 py-3 text-[#6C757E] font-bold">Block</p>
         <p className="w-2/12 py-3 text-[#6C757E] font-bold">Age</p>
         <p className="w-1/12 py-3 text-[#6C757E] font-bold">Txn</p>
-        <p className="w-3/12 py-3 text-[#6C757E] font-bold">Fee Reciptant</p>
+        <p className="w-3/12 py-3 text-[#6C757E] font-bold">Fee Recipient</p>
         <p className="w-2/12 py-3 text-[#6C757E] font-bold">Gas Used</p>
         <p className="w-2/12 py-3 text-[#6C757E] font-bold">Gas Limit</p>
         <p className="w-2/12 py-3 text-[#6C757E] font-bold">Base Fee</p>
       </div>
       {blocks}
+      <div className="flex justify-between mt-4">
+        <div>
+          <button
+            onClick={() => setCurrentPage((prev) => Math.max(prev - 1, 1))}
+            disabled={currentPage === 1}
+            className={`px-4 py-2 mr-2 mt-2 text-white ${
+              currentPage === 1 ? "bg-gray-500" : "bg-blue-500"
+            } rounded`}
+          >
+            Previous
+          </button>
+          <button
+            onClick={() =>
+              setCurrentPage((prev) => Math.min(prev + 1, totalPages))
+            }
+            disabled={currentPage === totalPages}
+            className={`px-4 py-2 mt-2 text-white ${
+              currentPage === totalPages ? "bg-gray-500" : "bg-blue-500"
+            } rounded`}
+          >
+            Next
+          </button>
+        </div>
+        <div className="flex items-center mt-2">
+          <span className="mr-2">Page</span>
+          <select
+            value={currentPage}
+            onChange={(e) => setCurrentPage(Number(e.target.value))}
+            className="px-2 py-1 border rounded"
+          >
+            {Array.from({ length: totalPages }, (_, i) => i + 1).map((page) => (
+              <option key={page} value={page}>
+                {page}
+              </option>
+            ))}
+          </select>
+          <span className="ml-2">of {totalPages}</span>
+        </div>
+      </div>
     </section>
   );
 }
--- src/components/Transactions.jsx
+++ src/components/Transactions.jsx
@@ -1,62 +1,100 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { DataContext } from "../context/DataContext";
 
 function Transactions() {
-    const { blocksData, transactions } = useContext(DataContext);
+  const { blocksData, transactions } = useContext(DataContext);
 
-    const location = useLocation();
-    // access the search parameters from the location object
-    const searchParams = new URLSearchParams(location.search);
-    // access a specific parameter by name
-    const blockNumber = searchParams.get("block");
+  const location = useLocation();
+  const searchParams = new URLSearchParams(location.search);
+  const blockNumber = searchParams.get("block");
 
-    return (
-        <section className="px-8 py-4 mx-24 my-8 bg-white border divide-y rounded-lg">
-            <div className="flex py-1 bg-sky-50">
-                <p className="w-3/12 py-3 text-[#6C757E] font-bold">Txn Hash</p>
-                <p className="w-2/12 py-3 text-[#6C757E] font-bold">Block</p>
-                <p className="w-3/12 py-3 text-[#6C757E] font-bold">From</p>
-                <p className="w-3/12 py-3 text-[#6C757E] font-bold">To</p>
-                <p className="w-2/12 py-3 text-[#6C757E] font-bold">Data</p>
-            </div>
-            {blockNumber
-                ? transactionsToComponent(
-                      blocksData.find(
-                          (blockData) => blockData.number == blockNumber
-                      ).transactions
-                  )
-                : transactionsToComponent(transactions)}
-        </section>
-    );
+  const itemsPerPage = 10;
+  const totalPages = Math.ceil(transactions.length / itemsPerPage);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+
+  const paginatedTransactions = blockNumber
+    ? blocksData
+        .find((blockData) => blockData.number == blockNumber)
+        .transactions.slice(startIndex, endIndex)
+    : transactions.slice(startIndex, endIndex);
+
+  return (
+    <section className="px-8 py-4 mx-24 my-8 bg-white border divide-y rounded-lg">
+      <div className="flex py-1 bg-sky-50">
+        <p className="w-3/12 py-3 text-[#6C757E] font-bold">Txn Hash</p>
+        <p className="w-2/12 py-3 text-[#6C757E] font-bold">Block</p>
+        <p className="w-3/12 py-3 text-[#6C757E] font-bold">From</p>
+        <p className="w-3/12 py-3 text-[#6C757E] font-bold">To</p>
+        <p className="w-2/12 py-3 text-[#6C757E] font-bold">Data</p>
+      </div>
+      {transactionsToComponent(paginatedTransactions)}
+      <div className="flex justify-between mt-4">
+        <div>
+          <button
+            onClick={() => setCurrentPage((prev) => Math.max(prev - 1, 1))}
+            disabled={currentPage === 1}
+            className={`px-4 py-2 mr-2 mt-2 text-white ${
+              currentPage === 1 ? "bg-gray-500" : "bg-blue-500"
+            } rounded`}
+          >
+            Previous
+          </button>
+          <button
+            onClick={() =>
+              setCurrentPage((prev) => Math.min(prev + 1, totalPages))
+            }
+            disabled={currentPage === totalPages}
+            className={`px-4 py-2 mt-2 text-white ${
+              currentPage === totalPages ? "bg-gray-500" : "bg-blue-500"
+            } rounded`}
+          >
+            Next
+          </button>
+        </div>
+        <div>
+          <span className="mr-2">Page</span>
+          <select
+            value={currentPage}
+            onChange={(e) => setCurrentPage(Number(e.target.value))}
+            className="px-2 py-1 mt-2 border rounded"
+          >
+            {Array.from({ length: totalPages }, (_, i) => i + 1).map((page) => (
+              <option key={page} value={page}>
+                {page}
+              </option>
+            ))}
+          </select>
+          <span className="ml-2">of {totalPages}</span>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 function transactionsToComponent(txs) {
-    return txs.map((tx, index) => {
-        return (
-            <section key={index} className="flex py-4">
-                <p className="w-3/12 text-[#357BAD]">
-                    <Link to={`/tx/${tx.hash}`}>{tx.hash.slice(0, 16)}...</Link>
-                </p>
-                <p className="w-2/12 text-[#357BAD]">
-                    <Link to={`/block/${tx.blockNumber}`}>
-                        {tx.blockNumber}
-                    </Link>
-                </p>
-                <p className="w-3/12 text-[#357BAD]">
-                    <Link to={`/address/${tx.from}`}>
-                        {tx.from.slice(0, 16)}...
-                    </Link>
-                </p>
-                <p className="w-3/12 text-[#357BAD]">
-                    <Link to={`/address/${tx.to}`}>
-                        {tx.to.slice(0, 16)}...
-                    </Link>
-                </p>
-                <p className="w-2/12">{tx.data.slice(0, 16)}...</p>
-            </section>
-        );
-    });
+  return txs.map((tx, index) => {
+    return (
+      <section key={index} className="flex py-4">
+        <p className="w-3/12 text-[#357BAD]">
+          <Link to={`/tx/${tx.hash}`}>{tx.hash.slice(0, 16)}...</Link>
+        </p>
+        <p className="w-2/12 text-[#357BAD]">
+          <Link to={`/block/${tx.blockNumber}`}>{tx.blockNumber}</Link>
+        </p>
+        <p className="w-3/12 text-[#357BAD]">
+          <Link to={`/address/${tx.from}`}>{tx.from.slice(0, 16)}...</Link>
+        </p>
+        <p className="w-3/12 text-[#357BAD]">
+          <Link to={`/address/${tx.to}`}>{tx.to.slice(0, 16)}...</Link>
+        </p>
+        <p className="w-2/12">{tx.data.slice(0, 16)}...</p>
+      </section>
+    );
+  });
 }
 
 export default Transactions;
